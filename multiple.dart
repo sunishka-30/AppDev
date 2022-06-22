45f931f12896e0890a8cda1583877b9884595df2
Sunishka Sharma <sunishka2002@gmail.com>
2022-06-22 21:37:49 +0530

    practice

---
+++ multiple.dart
@@ -0,0 +1,16 @@
+void main() {
+  List list = [1, 2, 3];
+  list.add(4);
+  list.add(5);
+  list.add(6);
+  list.add(7);
+  list.add(8);
+  list.add(9);
+  list.add(10);
+
+  list.forEach((i)
+  {
+    print(5 * i);
+  }
+  );
+}
